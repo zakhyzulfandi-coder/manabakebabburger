47e4ce1e8943603af256ecb9a2943606bfab3c2e
zakhyzulfandi-coder <zakhyzulfandi@gmail.com>
2026-06-06 09:55:10 +0800

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -3,7 +3,7 @@
  * Ganti appsScriptUrl dengan URL Web App Google Apps Script yang berakhiran /exec.
  */
 window.MANABA_PWA_CONFIG = {
-  appsScriptUrl: 'GANTI_DENGAN_URL_WEB_APP_APPS_SCRIPT_ANDA',
+  appsScriptUrl: 'https://script.google.com/a/~/macros/s/AKfycbxE-bpS6hIKyYbrid_kDIxj9gIbTfTmz6Gz1Ha7atgBauSUAqu2Mr4S8cgV3vng7PDl/exec',
   maxLoadingMs: 7000,
 
   // false = lebih cepat. true = selalu paksa reload iframe Apps Script.
